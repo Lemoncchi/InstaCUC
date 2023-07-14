96a7f2b934133f8243c922a7d813ce1e50689750
Mr.Nobody <mr.nobody.lim.l@gmail.com>
2023-07-14 20:38:54 +0800

    原开源项目

---
+++ sayhello/models.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+from sayhello import db
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    body = db.Column(db.String(200))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
