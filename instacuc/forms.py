02d8c034dd45b58d70d0e178e75397aa443bf36a
Mr.Nobody <mr.nobody.lim.l@gmail.com>
2023-07-15 15:34:20 +0800

    由于目前水印算法有误码，在用户输入时确认其「隐藏信息」输入为 ASCII 码

--- instacuc/forms.py
+++ instacuc/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm, file
 from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 from wtforms import StringField, SubmitField, TextAreaField, FileField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, ValidationError
 
 from instacuc import app
 
@@ -9,10 +9,14 @@ photos = UploadSet('photos', IMAGES)
 configure_uploads(app, photos)
 patch_request_class(app)  # set maximum file size, default is 16MB
 
+def ascii_only(form, field):
+    if not field.data.isascii():
+        raise ValidationError('由于目前使用的水印算法会有部分误码，中文编码嵌入后解码效果惨不忍睹，所以目前只能输入英文 (⋟﹏⋞)')
+
 class HelloForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(1, 20)])
     body = TextAreaField('Message', validators=[DataRequired(), Length(1, 200)])
-    hidden_message = TextAreaField('Hidden Message', validators=[DataRequired(), Length(1, 200)])
+    hidden_message = TextAreaField('Hidden Message', validators=[DataRequired(), Length(1, 200), ascii_only])
     photo = FileField(u'图片', validators=[
         file.FileAllowed(photos, u'只能上传图片！'), 
         file.FileRequired(u'文件未选择！')
